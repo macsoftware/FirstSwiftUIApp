003b4a9c967ee3087dc69dfac7e23ccca7286c76
macsoftware <87543117+macsoftware@users.noreply.github.com>
2023-01-26 14:55:24 +0000

    Initial Commit

---
+++ FirstSwiftUIApp/FirstSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftUIAppApp.swift
+//  FirstSwiftUIApp
+//
+//  Created by Ian MacKinnon on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
